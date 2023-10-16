abbab7575dfc82bdda4be252a998b4ee9262de25
Abdishakur Hassan Mohamed <102626624+abdishakuur366@users.noreply.github.com>
2023-10-16 02:16:14 -0700

    Last Update

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -47,7 +47,7 @@ function MyProfile(props) {
     <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
         <ul className="flex flex-wrap -mb-px">
             <li className="mr-2">
-            <Link to="/" className={`inline-block p-4 ${location.pathname === "/" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Followers</Link>
+            <Link to="/followers" className={`inline-block p-4 ${location.pathname === "/followers" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Followers</Link>
             </li>
             <li className="mr-2">
             <Link to="/following" className={`inline-block p-4 ${location.pathname === "/following" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Following</Link>
